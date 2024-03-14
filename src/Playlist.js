4ef2b9de437bdb09562d295a0ba9397a12416c41
JackEH2112 <jacke_hughes@hotmail.co.uk>
2024-03-14 16:38:06 +0000

    401 error fix. Playlist upload includes track(s) as expected.

--- src/Playlist.js
+++ src/Playlist.js
@@ -2,7 +2,6 @@ import React, {useState} from "react";
 import axios from "axios";
 import Track from "./Track";
 import './Playlist.css'
-//import { json } from "react-router-dom";
 
 function Playlist(props) {
     const {playlist, removeFromPlaylist, token} = props;
@@ -33,26 +32,6 @@ function Playlist(props) {
             });
             const USER_ID = responseUserId.data.id;
 
-            //Upload playlist to Spotify
-            /*await axios({
-                method: 'post',
-                url: `https://api.spotify.com/v1/users/${USER_ID}/playlists`,
-                headers: {'Authorization': `Bearer ${token}`},
-                data: {
-                    'name': `${playlistName}`,
-                    'description': 'Playlist created with JAMMMING app',
-                    'public': false
-                }
-            })
-            .then(async function (response) {
-                playlistId = response.data.id;
-                console.log(playlistId)
-            })
-            .catch(function (error) {
-                console.log(error);
-                alert(error)
-            });*/
-
             const uploadPlaylist = async () => {
                 try {
                     const response = await axios.post(`https://api.spotify.com/v1/users/${USER_ID}/playlists`,
@@ -64,8 +43,6 @@ function Playlist(props) {
                         {headers: {'Authorization': `Bearer ${token}`}}
                     );
                     playlistId = response.data.id;
-                    console.log(`Playlist upload response:`)
-                    console.log(response);
                     //Get track uris
                     const trackPostUrl = `https://api.spotify.com/v1/playlists/${playlistId}/tracks`
                     for (let i = 0; i < playlist.length; i++){
@@ -74,76 +51,31 @@ function Playlist(props) {
                     //Post uris to playlist
                     const uploadTracks = async () => {
                         try {
-                            const response = await axios.post(`${trackPostUrl}`,{},
+                            const response = await axios.post(`${trackPostUrl}`,
                                 {
                                     "uris": uriArray, 
-                                    "postition": 0
+                                    "position": 0
                                 },
                                 {headers: {'Authorization' : `Bearer ${token}`}}
                             );
-                            console.log(`Tracks to playlist response:`);
                             console.log(response)
                             //reset
                             alert(`Upload ${playlistName} successful`)
-                            setPlaylists(playlists => [...playlists, playlistName])
                             setPlayListName('')
                             playlist.splice(0)
                             playlistId = '';
                         }
                         catch(error){
-                            console.log(`Tracks to playlist error:`);
-                            if(error.response){
-                                console.log(error.response)
-                            }
-                            else if(error.request){
-                                console.log(error.request)
-                            }
-                            else if(error.message){
-                                console.log(error.message)
-                            }
+                            console.log(error)
                         }
                     }
-                    /*await axios({
-                        method: "post",
-                        url: `${trackPostUrl}`,
-                        headers: {"Authorization": `Bearer ${token}`, "Content-Type": 'application/json'},
-                        data: {
-                            
-                        }
-                    })
-                    .then(function (response) {
-                        console.log(response)  
-                        //reset
-                        alert(`Upload ${playlistName} successful`)
-                        setPlaylists(playlists => [...playlists, playlistName])
-                        setPlayListName('')
-                        playlist.splice(0)
-                    })
-                    .catch(function (error) {
-                        if(error.response){
-                            console.log(error.response.config.data)
-                        }
-                        else if(error.request){
-                            console.log(error.request)
-                        }
-                        else if(error.message){
-                            console.log(error.message)
-                        }
-                        else{
-                            console.log('Good luck')
-                        }
-                    })*/
-                    
                     uploadTracks();
                 }
                 catch(error){
                     console.log(error);
                 }
             }
-
             uploadPlaylist();
-
-            
         }
     }
 
